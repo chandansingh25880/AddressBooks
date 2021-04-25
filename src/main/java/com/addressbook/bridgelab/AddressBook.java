f8aae39c82e48f5196735ed2bbed58a654be860f
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-04-26 01:19:21 +0530

    Added: To Add new contacts in addressbook

--- src/main/java/com/addressbook/bridgelab/AddressBook.java
+++ src/main/java/com/addressbook/bridgelab/AddressBook.java
@@ -1,5 +1,7 @@
 package com.addressbook.bridgelab;
 
+import java.util.Scanner;
+
 /* @Description - To create a contacts in address book with first name, last name, address, city, state,
  * zip,mobile number.*/
 public class AddressBook {
@@ -11,6 +13,29 @@ public class AddressBook {
         System.out.println(contact.toString());
 
     }
-}
 
+    /* @Description- Add new contacts in address book  */
+        private void addContact() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the first name");
+        String firstName = scanner.nextLine();
+        System.out.println("enter the last name");
+        String lastName = scanner.nextLine();
+        System.out.println("enter the address");
+        String address = scanner.nextLine();
+        System.out.println("enter the state");
+        String state = scanner.nextLine();
+        System.out.println("enter the city");
+        String city = scanner.nextLine();
+        System.out.println("enter the zip code");
+        int zipCode = scanner.nextInt();
+        System.out.println("enter the mobile number");
+        long number = scanner.nextLong();
+        System.out.println("enter email-id");
+        String email = scanner.next();
+        Contact contact = new Contact(firstName, lastName, address, state, city, zipCode, number, email);
+        System.out.println("contact successfully added");
+
+    }
+}
 
