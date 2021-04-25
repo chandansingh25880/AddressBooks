c63af428890e9f26d9a07f5f95d1c4e766a1b62d
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-04-26 01:03:32 +0530

    Added: Welcome to the AddressBook Program

---
+++ src/main/java/com/addressbook/bridgelab/AddressBook.java
@@ -0,0 +1,12 @@
+package com.addressbook.bridgelab;
+
+/* @Description - To create a contacts in address book with first name, last name, address, city, state,
+ * zip,mobile number.*/
+public class AddressBook {
+    public static void main(String[] args) {
+        System.out.println("Welcome to address book system program");
+
+    }
+}
+
+
---
+++ src/main/java/com/addressbook/bridgelab/Contact.java
@@ -0,0 +1,7 @@
+package com.addressbook.bridgelab;
+
+/* @Description - To create a contacts in address book with first name, last name, address, city, state,
+ * zip,mobile number. */
+public class Contact {
+
+}
